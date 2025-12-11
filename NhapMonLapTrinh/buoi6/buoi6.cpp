9b49e7555a99d9f508cec4cf885d64eee514929c
buoi6 <Administrator@PMT2003>
2025-12-11 14:34:12 +0700

    baio tap buoi 6

--- NhapMonLapTrinh/buoi6/buoi6.cpp
+++ NhapMonLapTrinh/buoi6/buoi6.cpp
@@ -29,9 +29,17 @@ void cau3() {
     }
     cout << "Tich:" << tich << endl;
 }
+void cau4() {
+    int n;
+    cout << "Nhap bang cuu chuong:";
+    cin >> n;
+    for (int i = 1; i <= 10; i++) {
+        cout << n << "x" << " = " << n * i << endl;
+    }
+}
 int main() {
 
-    cau3();
+    cau4();
 }
 
 
