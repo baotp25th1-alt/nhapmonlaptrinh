18439cbb45874bc5c01cbab549d4701170dd4d30
buoi6 <Administrator@PMT2003>
2025-12-11 13:17:31 +0700

    bai tap buoi 6

---
+++ NhapMonLapTrinh/buoi6/buoi6.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++) {
+        cout << "Hello:" << i << endl;
+   }
+    int j = 1;
+    while (j <= 5) {
+        cout << "While Hello:" << j << endl;
+        j++;
+
+    }
+    int k = 1;
+    do {
+        cout << "Do..While Hello:" << k << endl;
+        k++;
+    } while (k <= 5);
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+
