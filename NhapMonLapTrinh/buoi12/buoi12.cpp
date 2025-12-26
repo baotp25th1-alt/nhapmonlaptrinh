b19a02e96e9c899ea2bb5c188e697c804aeaf19a
Buoi 12 <Administrator@PMT2003>
2025-12-26 10:47:57 +0700

    bai tap buoi 12

--- NhapMonLapTrinh/buoi12/buoi12.cpp
+++ NhapMonLapTrinh/buoi12/buoi12.cpp
@@ -1,5 +1,7 @@
 #include "Bibotr.h"
-#define MAX 100
+#include <iostream>
+using namespace std;
+#define MAX 100        
 
 int main()
 {
@@ -12,8 +14,15 @@ int main()
    // vitrinhonhat(a, n);
    // nguyentonhohon100(a, n);
    // tonggiatriam(a, n);
-    sapxep(a, n);
-    output(a, n);
+   // sapxep(a, n);
+   // output(a, n);
+    int kq = giatriduongdautien(a, n);
+    if (kq == 0) {
+        cout << "Khong co gia tri duong trong mang" << endl;
+    }
+    else {
+        cout << "Gia tri duong dau tien:" << kq << endl;
+    }
 }
 
 
